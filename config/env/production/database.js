121a99768aafb7a1a7a0c38283f4d2cc2c4ad244
Giovani Carnaval <gi.carnaval91@gmail.com>
2022-10-22 00:35:19 -0300

    Atualizando conexão com a database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,12 +1,18 @@
-// module.exports = ({ env }) => ({
-//     connection: {
-//       client: 'postgres',
-//       connector: 'bookshelf',
-//       connection:{
-//         socketPath: `/cloudsql/${env('INSTANCE_CONNECTION_NAME')}`,
-//         database: env('DATABASE_NAME'),
-//         user: env('DATABASE_USERNAME'),
-//         password: env('DATABASE_PASSWORD'),
-//       }
-//     }
-//   });
+const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
+      },
+    });
